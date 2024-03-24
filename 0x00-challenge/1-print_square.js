51378f8a9936ac2781af9801eb8f6a88c8dabb9e
fordstile <masterland9772@gmail.com>
2024-03-24 01:25:29 -0700

    debugging

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/*
+    Prints a square using the character #
+
+    The size of the square is provided as the first argument 
+    of the program.
+*/
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1);
+}
+
+size = parseInt(process.argv[2], 10);
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
